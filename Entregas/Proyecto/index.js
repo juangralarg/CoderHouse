5b38d688e49596ff999824820d68cb775d52861c
juan.grana <juan.andres.g@outlook.com>
2024-05-22 11:04:37 -0300

    Codigo para primera entega 11:04AM 20240522

---
+++ Entregas/Proyecto/index.js
@@ -0,0 +1,6 @@
+/* let option = true;
+while(option !=0 ){
+    option = prompt("Opcion: \n 1. Ver saldo. \n 2.Extraer Saldo \n 0.Salir");
+    //se ejecutaria ifiito salvo que la condicion se torne falsa
+    condition = 10==20; //Da false y para el while
+} */
