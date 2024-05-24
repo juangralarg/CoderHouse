0e2d2f7e7bf8af6f755e5051f01a41ad6d9b963f
juan.grana <juan.andres.g@outlook.com>
2024-05-24 13:57:41 -0300

    Creacion Carpeta CLASE 6

---
+++ Entregas/Entrega2/index.js
@@ -0,0 +1,23 @@
+//SEGUNDA ENTREGA
+//ARRAY
+
+//-------------------------------------------------
+//FUNCION ingreso datos
+//-------------------------------------------------
+/* 
+function ingreso (){
+const lista = [ "Nombre: ",  "Apellido: ", "Cedula: " ];
+console.log(lista.values); */
+
+  
+/*     for (let value of lista) {
+        dato = prompt ("Ingrese :");
+        value += 1;
+        console.log(value);
+      }*/
+/* } 
+
+
+
+ingreso();
+ */
