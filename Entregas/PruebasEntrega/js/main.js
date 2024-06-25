3293aef5c861ea001c7c5c54afbee61f878308a3
Juan Andrés Graña <juan.andres.g@outlook.com>
2024-06-25 09:44:09 -0300

    commit

--- Entregas/PruebasEntrega/js/main.js
+++ Entregas/PruebasEntrega/js/main.js
@@ -56,7 +56,7 @@
         function mostrarLogin() {
             let docuLogin = document.getElementById('contenido');
             document.getElementById('contenido').innerHTML =`
- <div id="formlogin"><img src="/img/logo.jpg" alt="Logo">
+ <div id="formlogin"><img src="./img/logo.jpg" alt="Logo">
                 <BR><h2>Iniciar Sesión</h2>
                 <form class="formSubmit" action="#">
                         <input type="text" id="GetUsuario" placeholder="Usuario" required>
@@ -84,7 +84,7 @@
         let docuRegister= document.getElementById('contenido');
         event.preventDefault()
             document.getElementById('contenido').innerHTML = `            
-             <div id="formlogin"><img src="/img/logo.jpg" alt="Logo">
+             <div id="formlogin"><img src="./img/logo.jpg" alt="Logo">
                 
                 <h2>Crea tu cuenta</h2>
                 <form class="formSubmit" action="#">
