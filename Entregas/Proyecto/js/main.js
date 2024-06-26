cb1313a6ffe1832ff4e44d72aabb31bcc79050da
Juan Andrés Graña <juan.andres.g@outlook.com>
2024-06-26 08:59:39 -0300

    2024626_002

--- Entregas/Proyecto/js/main.js
+++ Entregas/Proyecto/js/main.js
@@ -119,7 +119,7 @@
            let usr=document.querySelector('#GetUsuario').value;//Toma valor ingresado en campo Usuario
            let pas=document.querySelector('#GetPass').value;//Toma valor ingresado en campo PAssword
            let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];//Lee localstorage para tomar informacion del primer USR y PASS, si no hay valores retorna Array Vacio (derecha del OR)
-           
+           event.preventDefault()
            let [[{ usuario, pass }]]= usuarios; //Desestructura informacion para tomar valores del primer USR Y PASS
         
 
